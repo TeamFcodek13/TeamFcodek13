c21bd287c0aebd1360ee9bc7a4914d8be697c206
Tien <tien01662892891@gmail.com>
2018-05-02 09:54:53 +0700

    create account

---
+++ ProjectJava/src/mystore/AccountManager.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mystore;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author IMMORTALITY IPOS
+ */
+public class AccountManager implements Serializable {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AccountManager(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+    
+}
